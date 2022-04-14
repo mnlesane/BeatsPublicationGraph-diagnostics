780d068feea2d5313f3b9322f18033af6f0fa536
root <root@lasc.home>
2022-04-14 10:47:58 -0500

    state snapshot before pulling update

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,6 +6,7 @@ import {
   json,
   JSONValueKind,
   ByteArray,
+  Bytes
 } from "@graphprotocol/graph-ts";
 import { PostCreated } from "../generated/PublishingLogic/PublishingLogic";
 import { PublicationEntity, MetadataEntity } from "../generated/schema";
@@ -24,7 +25,7 @@ export function handlePublication(event: PostCreated): void {
   entity.pubId = event.params.pubId.toHex();
   entity.contentURI = event.params.contentURI.toString();
   entity.timestamp = event.params.timestamp.toString();
-  const contentURI = event.params.contentURI.toString();
+  let contentURI = event.params.contentURI.toString();
 
   if (
     contentURI.includes('"traitType":"Genre"') &&
@@ -35,63 +36,63 @@ export function handlePublication(event: PostCreated): void {
     entity.save();
   }
 
-  // const data = event.params.contentURI.toString();
-  // log.info("Hello 1 ************* {}", [data]);
-  // if (!data) return;
-  // const value = json.try_fromString(data);
-  // log.info("Hello a ************{}", [typeof value]);
-  // if (!value) {
-  //   return;
-  // }
-  // if (!value.isOk) {
-  //   return;
-  // }
-  // log.info("Hello 2 *************", []);
-  // if (value.value.kind !== JSONValueKind.OBJECT) {
-  //   return;
-  // }
-  // log.info("Hello 3 *************", []);
-  // let parsedObj = value.value.toObject();
-  // if (!parsedObj) {
-  //   log.info("Hello 4 ************", []);
-  //   return;
-  // }
-  // const metadataId = parsedObj.get("metadata_id");
-  // if (!metadataId) {
-  //   log.info("Unable to get metadata ID.", []);
-  //   return;
-  // }
-  // const attributes = parsedObj.get("attributes");
-  // if (!attributes) {
-  //   log.info("Unable to get attributes.", []);
-  //   return;
-  // }
-  // const attributeArray = attributes.toArray();
-  // const genre = attributeArray[0].toString();
-  // const bpm = attributeArray[1].toI64();
-  // const keyScale = attributeArray[2].toString();
-  // const type = attributeArray[3].toString();
-  // if (!genre) {
-  //   log.info("Unable to get genre.", []);
-  //   return;
-  // }
-  // if (!bpm) {
-  //   log.info("Unable to get bpm.", []);
-  //   return;
-  // }
-  // if (!keyScale) {
-  //   log.info("Unable to get keyscale.", []);
-  //   return;
-  // }
-  // if (!type) {
-  //   log.info("Unable to get type.", []);
-  //   return;
-  // }
+  let data = event.params.contentURI.toString();
+  log.info("Hello 1 ************* {}", [data]);
+  if (!data) return;
+  let value = json.try_fromBytes(changetype<Bytes>(data));
+  log.info("Hello a ************{}", [typeof value]);
+  if (!value) {
+    return;
+  }
+  if (!value.isOk) {
+    return;
+  }
+  log.info("Hello 2 *************", []);
+  if (value.value.kind !== JSONValueKind.OBJECT) {
+    return;
+  }
+  log.info("Hello 3 *************", []);
+  let parsedObj = value.value.toObject();
+  if (!parsedObj) {
+    log.info("Hello 4 ************", []);
+    return;
+  }
+  let metadataId = parsedObj.get("metadata_id");
+  if (!metadataId) {
+    log.info("Unable to get metadata ID.", []);
+    return;
+  }
+  let attributes = parsedObj.get("attributes");
+  if (!attributes) {
+    log.info("Unable to get attributes.", []);
+    return;
+  }
+  let attributeArray = attributes.toArray();
+  let genre = attributeArray[0].toString();
+  let bpm = attributeArray[1].toI64();
+  let keyScale = attributeArray[2].toString();
+  let type = attributeArray[3].toString();
+  if (!genre) {
+    log.info("Unable to get genre.", []);
+    return;
+  }
+  if (!bpm) {
+    log.info("Unable to get bpm.", []);
+    return;
+  }
+  if (!keyScale) {
+    log.info("Unable to get keyscale.", []);
+    return;
+  }
+  if (!type) {
+    log.info("Unable to get type.", []);
+    return;
+  }
 
-  // let metadataEntity = MetadataEntity.load(metadataId.toString());
-  // if (!metadataEntity) {
-  //   metadataEntity = new MetadataEntity(metadataId.toString());
-  // }
-  // metadataEntity.save();
-  // entity.metadata = metadataId.toString();
+  let metadataEntity = MetadataEntity.load(metadataId.toString());
+  if (!metadataEntity) {
+    metadataEntity = new MetadataEntity(metadataId.toString());
+  }
+  metadataEntity.save();
+  entity.metadata = metadataId.toString();
 }
