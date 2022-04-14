4b30c2923b285360af6151dedbd838513ae98ca7
AIRESH BHAT <nelliairesh@gmail.com>
2022-04-14 16:10:21 +0530

    Update mapping function, update contract address and starting block number

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,6 +6,7 @@ import {
   json,
   JSONValueKind,
   ByteArray,
+  Bytes,
 } from "@graphprotocol/graph-ts";
 import { PostCreated } from "../generated/PublishingLogic/PublishingLogic";
 import { PublicationEntity, MetadataEntity } from "../generated/schema";
@@ -26,72 +27,72 @@ export function handlePublication(event: PostCreated): void {
   entity.timestamp = event.params.timestamp.toString();
   const contentURI = event.params.contentURI.toString();
 
-  if (
-    contentURI.includes('"traitType":"Genre"') &&
-    contentURI.includes('"traitType":"Beats Per Minute"') &&
-    contentURI.includes('"traitType":"Key Scale"') &&
-    contentURI.includes('"traitType":"Beat Type"')
-  ) {
-    entity.save();
+  // if (
+  //   contentURI.includes('"traitType":"Genre"') &&
+  //   contentURI.includes('"traitType":"Beats Per Minute"') &&
+  //   contentURI.includes('"traitType":"Key Scale"') &&
+  //   contentURI.includes('"traitType":"Beat Type"')
+  // ) {
+  //   entity.save();
+  // }
+
+  const data = event.params.contentURI.toString();
+  log.info("Hello 1 ************* {}", [data]);
+  if (!data) return;
+  const value = json.try_fromString(data);
+  log.info("Hello a ************{}", [typeof value]);
+  if (!value) {
+    return;
+  }
+  if (!value.isOk) {
+    return;
+  }
+  log.info("Hello 2 *************", []);
+  if (value.value.kind !== JSONValueKind.OBJECT) {
+    return;
+  }
+  log.info("Hello 3 *************", []);
+  let parsedObj = value.value.toObject();
+  if (!parsedObj) {
+    log.info("Hello 4 ************", []);
+    return;
+  }
+  const metadataId = parsedObj.get("metadata_id");
+  if (!metadataId) {
+    log.info("Unable to get metadata ID.", []);
+    return;
+  }
+  const attributes = parsedObj.get("attributes");
+  if (!attributes) {
+    log.info("Unable to get attributes.", []);
+    return;
+  }
+  const attributeArray = attributes.toArray();
+  const genre = attributeArray[0].toString();
+  const bpm = attributeArray[1].toI64();
+  const keyScale = attributeArray[2].toString();
+  const type = attributeArray[3].toString();
+  if (!genre) {
+    log.info("Unable to get genre.", []);
+    return;
+  }
+  if (!bpm) {
+    log.info("Unable to get bpm.", []);
+    return;
+  }
+  if (!keyScale) {
+    log.info("Unable to get keyscale.", []);
+    return;
+  }
+  if (!type) {
+    log.info("Unable to get type.", []);
+    return;
   }
 
-  // const data = event.params.contentURI.toString();
-  // log.info("Hello 1 ************* {}", [data]);
-  // if (!data) return;
-  // const value = json.try_fromString(data);
-  // log.info("Hello a ************{}", [typeof value]);
-  // if (!value) {
-  //   return;
-  // }
-  // if (!value.isOk) {
-  //   return;
-  // }
-  // log.info("Hello 2 *************", []);
-  // if (value.value.kind !== JSONValueKind.OBJECT) {
-  //   return;
-  // }
-  // log.info("Hello 3 *************", []);
-  // let parsedObj = value.value.toObject();
-  // if (!parsedObj) {
-  //   log.info("Hello 4 ************", []);
-  //   return;
-  // }
-  // const metadataId = parsedObj.get("metadata_id");
-  // if (!metadataId) {
-  //   log.info("Unable to get metadata ID.", []);
-  //   return;
-  // }
-  // const attributes = parsedObj.get("attributes");
-  // if (!attributes) {
-  //   log.info("Unable to get attributes.", []);
-  //   return;
-  // }
-  // const attributeArray = attributes.toArray();
-  // const genre = attributeArray[0].toString();
-  // const bpm = attributeArray[1].toI64();
-  // const keyScale = attributeArray[2].toString();
-  // const type = attributeArray[3].toString();
-  // if (!genre) {
-  //   log.info("Unable to get genre.", []);
-  //   return;
-  // }
-  // if (!bpm) {
-  //   log.info("Unable to get bpm.", []);
-  //   return;
-  // }
-  // if (!keyScale) {
-  //   log.info("Unable to get keyscale.", []);
-  //   return;
-  // }
-  // if (!type) {
-  //   log.info("Unable to get type.", []);
-  //   return;
-  // }
-
-  // let metadataEntity = MetadataEntity.load(metadataId.toString());
-  // if (!metadataEntity) {
-  //   metadataEntity = new MetadataEntity(metadataId.toString());
-  // }
-  // metadataEntity.save();
-  // entity.metadata = metadataId.toString();
+  let metadataEntity = MetadataEntity.load(metadataId.toString());
+  if (!metadataEntity) {
+    metadataEntity = new MetadataEntity(metadataId.toString());
+  }
+  metadataEntity.save();
+  entity.metadata = metadataId.toString();
 }
